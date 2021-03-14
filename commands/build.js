d5fad8b0076944475b7a4310437295ab842db7d9
Scott Walters <sawalter@ncsu.edu>
2021-03-14 12:21:34 -0400

    Created pipeline build command

--- commands/build.js
+++ commands/build.js
@@ -6,17 +6,18 @@ const fs = require('fs');
 
 const scpSync = require('../lib/scp');
 const sshSync = require('../lib/ssh');
+const [,, ... args] = process.argv;
 
-exports.command = 'build';
-exports.desc = 'Create the Jenkins Build Job';
-exports.builder = yargs => {
-    yargs.options({
-        privateKey: {
-            describe: 'Install the provided private key on the configuration server',
-            type: 'string'
-        }
-    });
-};
+//exports.command = 'build';
+//exports.desc = 'Create the Jenkins Build Job';
+//exports.builder = yargs => {
+//    yargs.options({
+//        privateKey: {
+//            describe: 'Install the provided private key on the configuration server',
+//            type: 'string'
+//        }
+//    });
+//};
 
 
 exports.handler = async argv => {
@@ -37,8 +38,13 @@ async function run(privateKey) {
     console.log(chalk.blueBright('Creating Jenkins Job for checkbox.io...'));
     result = sshSync('/bakerx/cm/create-checkio-build-job.sh', 'vagrant@192.168.33.20');
     if( result.error ) { console.log(result.error); process.exit( result.status ); }
-
+    
     console.log(chalk.blueBright('Starting checkbox.io Build Job...'));
+    result = sshSync('node /bakerx/cm/start-build.js', 'vagrant@192.168.33.20');
+    if( result.error ) { console.log(result.error); process.exit( result.status ); }
+
+    //console.log(chalk.blueBright('Starting checkbox.io Build Job...'));
+    //console.log(`Args: ${args}`);
     //result = sshSync(`/bakerx/cm/run-ansible.sh ${filePath} ${inventoryPath}`, 'vagrant@192.168.33.20');
     //if( result.error ) { process.exit( result.status ); }
 }
